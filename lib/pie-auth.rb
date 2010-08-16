587dd089bea5226bc1e21be7e553fe9ca883fd36
chinachengliwen <chinachengliwen@gmail.com>
2010-08-16 15:30:51 +0800

    resset core_config initalize

--- lib/pie-auth.rb
+++ lib/pie-auth.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'pie-auth/authenticated_system'
 require 'pie-auth/core_service'
+CoreService.reset_config
 require 'pie-auth/build_database_connection'
 require 'pie-auth/set_readonly'
 require 'pie-auth/preference'
