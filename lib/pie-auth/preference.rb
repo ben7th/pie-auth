827d0c11fb474e663c4e3a9650ffc97f8db401fd
fushang318 <fushang318@gmail.com>
2010-08-18 14:04:36 +0800

    使 preference 支持测试

--- lib/pie-auth/preference.rb
+++ lib/pie-auth/preference.rb
@@ -1,3 +1,14 @@
 class Preference < ActiveRecord::Base
   build_database_connection(CoreService::USER_AUTH)
+
+  if RAILS_ENV == "test" && !self.table_exists?
+    self.connection.create_table :preferences, :force => true do |t|
+      t.integer  "user_id",                          :null => false
+      t.boolean  "auto_popup_msg", :default => true
+      t.datetime "created_at"
+      t.datetime "updated_at"
+      t.boolean  "show_tooltip",   :default => true
+      t.string   "theme"
+    end
+  end
 end
