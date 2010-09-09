b23519189ab859b45dd1848c86a54c646d2a58d9
fushang318 <fushang318@gmail.com>
2010-09-09 17:45:06 +0800

    优化 pin_url_for

--- lib/pie-auth/project_link_module.rb
+++ lib/pie-auth/project_link_module.rb
@@ -1,6 +1,22 @@
 module ProjectLinkModule
+  URLS = {
+    "user_auth"=>"http://2010.mindpin.com",
+    "discuss"=>"http://2010.mindpin.com/discuss",
+    "pin-workspace"=>"http://2010.mindpin.com/workspace",
+    "ui"=>"http://ui.mindpin.com"
+  }
+  
   def pin_url_for(project_name,path=nil)
+    if RAILS_ENV == "production"
+      path ||= ""
+      return File.join(find_site_by_name(project_name),path)
+    end
     path ||= ""
     File.join(CoreService.project(project_name).url,path)
   end
+
+  def find_site_by_name(project_name)
+    URLS[project_name] || "http://unknow"
+  end
+
 end
