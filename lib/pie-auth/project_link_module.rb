d18fb38140f3ca8689da1c059457617a26bde3f6
fushang318 <fushang318@gmail.com>
2010-09-17 11:12:42 +0800

    修改pin_url_for

--- lib/pie-auth/project_link_module.rb
+++ lib/pie-auth/project_link_module.rb
@@ -1,22 +1,31 @@
 module ProjectLinkModule
   URLS = {
-    "user_auth"=>"http://2010.mindpin.com/",
-    "discuss"=>"http://2010.mindpin.com/",
-    "pin-workspace"=>"http://2010.mindpin.com/",
-    "ui"=>"http://ui.mindpin.com/"
+    "prodeuction"=>{
+      "user_auth"=>"http://2010.mindpin.com/",
+      "discuss"=>"http://2010.mindpin.com/",
+      "pin-workspace"=>"http://2010.mindpin.com/",
+      "ui"=>"http://ui.mindpin.com/",
+      "pin-bugs"=>"http://2010.mindpin.com/"},
+    "development"=>{
+      "user_auth"=>"http://dev.2010.mindpin.com/",
+      "discuss"=>"http://dev.2010.mindpin.com/",
+      "pin-workspace"=>"http://dev.2010.mindpin.com/",
+      "ui"=>"http://dev.ui.mindpin.com/",
+      "pin-bugs"=>"http://dev.2010.mindpin.com/"},
+    "test"=>{
+      "user_auth"=>"http://dev.2010.mindpin.com/",
+      "discuss"=>"http://dev.2010.mindpin.com/",
+      "pin-workspace"=>"http://dev.2010.mindpin.com/",
+      "ui"=>"http://dev.ui.mindpin.com/",
+      "pin-bugs"=>"http://dev.2010.mindpin.com/"}
   }
   
   def pin_url_for(project_name,path=nil)
-    if RAILS_ENV == "production"
-      path ||= ""
-      return File.join(find_site_by_name(project_name),path)
-    end
-    path ||= ""
-    File.join(CoreService.project(project_name).url,path)
+    return File.join(find_site_by_name(project_name),path)
   end
 
   def find_site_by_name(project_name)
-    URLS[project_name] || "http://unknow"
+    URLS[RAILS_ENV][project_name] || "http://unknow"
   end
 
 end
